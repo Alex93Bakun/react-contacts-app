cbe9c86d7ad3c24c4da869aba5684c1090863224
Alex93Bakun <56162141+Alex93Bakun@users.noreply.github.com>
2021-08-29 14:01:01 +0300

    async await

--- src/pages/contacts/index.jsx
+++ src/pages/contacts/index.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Contacts = () => {
+    const [contacts, setContacts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        const getContacts = async () => {
+            try {
+                setIsLoading(true)
+                const response = await fetch("https://randomuser.me/api/?results=200");
+                const {results, error} = await response.json();
+                if (error) {
+                    throw new Error(error);
+                }
+                setContacts(results);
+                setIsError(false);
+            } catch (e) {
+                setIsError(true);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        getContacts();
+    }, []);
+
+
+    if (isLoading) {
+        return <div>...loading</div>
+    }
+
+    if (isError) {
+        return <div>...error</div>
+    }
+
     return (
         <div>
-            Contacts
+            {contacts[0].name.first}
         </div>
     );
 };
